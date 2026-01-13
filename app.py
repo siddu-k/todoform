ea01a645a7b7c4837cb6ef447f23eb83067602df
siddu <sridatta.k99@gmail.com>
2026-01-13 19:27:14 +0530

    master 2

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, render_template
+from pymongo import MongoClient
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+
+client = MongoClient(MONGO_URI, tls=True)
+
+db = client["todo_db"]
+collection = db["todo_items"]
+
+@app.route('/')
+def home():
+    return render_template("todo.html")
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    item_name = request.form['itemName']
+    item_description = request.form['itemDescription']
+
+    todo_data = {
+        "itemName": item_name,
+        "itemDescription": item_description
+    }
+
+    collection.insert_one(todo_data)
+
+    return "Todo item saved successfully!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
